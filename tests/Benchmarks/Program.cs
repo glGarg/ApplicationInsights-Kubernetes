100acf460e124fdf3f479ea958603c8c29086f57
Jacob Bovee (from Dev Box) <jabovee@microsoft.com>
2024-10-28 15:16:03 -0700

    Appended summary

--- tests/Benchmarks/Program.cs
+++ tests/Benchmarks/Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var config = ManualConfig.Create(DefaultConfig.Instance).WithArtifactsPath("artifacts");
+        var config = ManualConfig.Create(DefaultConfig.Instance).WithArtifactsPath("artifacts").WithOptions(ConfigOptions.JoinSummary);
         var stringUtilsSummary = BenchmarkRunner.Run<StringUtilsBenchmark>(config);
         var scrubberSummary = BenchmarkRunner.Run<ScrubberBenchmark>(config);
     }
