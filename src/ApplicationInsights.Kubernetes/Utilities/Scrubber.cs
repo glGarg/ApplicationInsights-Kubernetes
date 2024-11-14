8003110036e0923d7d2d69e98d6bea66a9b1a1dd
Spandan Garg <spandan.garg1706@gmail.com>
2024-11-14 12:55:24 -0800

    Update Scrubber.cs

--- src/ApplicationInsights.Kubernetes/Utilities/Scrubber.cs
+++ src/ApplicationInsights.Kubernetes/Utilities/Scrubber.cs
@@ -8,20 +8,23 @@ namespace Microsoft.ApplicationInsights.Kubernetes
 {
     using System;
     using System.Collections.Generic;
+    using System.Globalization;
     using System.Linq;
-    using System.Text;
-    using System.Text.RegularExpressions;
 
     public class Scrubber
     {
-        public const string EmailRegExPattern = @"[a-zA-Z0-9!#$+\-^_~]+(?:\.[a-zA-Z0-9!#$+\-^_~]+)*@(?:[a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}";
-        public static string ScrubData(string data, char replacementChar)
+        internal record LocalizedWord(string Text, CultureInfo Culture);
+        private static IEnumerable<LocalizedWord> DisallowedWords { get; } = ScrubberHelpers.LoadDisallowedWords();
+
+        public static string ScrubData(string data, char replacementChar, CultureInfo culture)
         {
-            Regex rx = new Regex(EmailRegExPattern);
-            foreach (Match match in rx.Matches(data))
+            List<string> wordList = DisallowedWords
+                .Where(word => culture.Equals(CultureInfo.InvariantCulture) || culture.Equals(word.Culture))
+                .Select(word => word.Text).ToList();
+
+            foreach (string word in wordList)
             {
-                string replacementString = new string(replacementChar, match.Value.Length);
-                data = data.Replace(match.Value, replacementString);
+                data = data.Replace(word, replacementChar.ToString(), ignoreCase: true, culture);
             }
 
             return data;
